04ab14b47919a5f5a7aa7411144aae8798594e35
Muhammad K <akhalid.code@gmail.com>
2021-04-22 12:05:58 -0700

    set up image upload route

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const fileRoutes = require('./routes/file-upload');
+// const fileRoutes = require('./routes/file-upload');
 const userRoutes = require('./routes/user-routes');
+const imageRoutes = require('./routes/image-upload');
 
 // express middleware, used to be bodyparser
 app.use(express.json());
@@ -14,14 +15,14 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // app.use(require('./routes'));
-app.use('/api/', fileRoutes);
+// app.use('/api/', fileRoutes);
 app.use('/api/', userRoutes);
+app.use('/api/', imageRoutes); // add this route for image upload
 
 // Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
-
 
 // const express = require('express');
 // const path = require('path');
